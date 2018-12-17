7cec2b9b13bd60e4688bdc56e2b5f2e1f08a3bb8
Ziyi-Guo <kwokjona@gmail.com>
2018-12-17 20:37:41 +0800

    change dir

--- ss-fly.sh
+++ ss-fly.sh
@@ -154,8 +154,10 @@ install_ssr() {
                      ;;
         esac
 	# wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
-	chmod +x shadowsocksR.sh
+	cd ss-fly/
+    chmod +x shadowsocksR.sh
 	./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+    cd ..
 }
 
 check_os_() {
